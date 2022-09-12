0a230d0ef2263ff815c7bccd6b725a43d513df96
hailemichael ayana <hmichaelmulugeta@gmail.com>
2022-09-12 02:33:55 -0700

    corrected capital and small letter error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	lastNum = n % 10;
 	if (lastNum > 5)
-		printf("last digit of %i is %i and is greater than 5\n", n, lastNum);
+		printf("Last digit of %i is %i and is greater than 5\n", n, lastNum);
 	else if (lastNum == 0)
 		printf("Last digit of %i is %i and is 0\n", n, lastNum);
 	else if (lastNum < 6 && lastNum != 0)
