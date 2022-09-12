082c4bb43031ed3eedee8b916e43f8f0647b4e4b
hailemichael ayana <hmichaelmulugeta@gmail.com>
2022-09-12 03:28:39 -0700

    ques 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print all single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int numbers = 0 ; numbers < 9 ; numbers++)
+		printf("%i", numbers);
+	printf("\n");
+	return (0);
+}
