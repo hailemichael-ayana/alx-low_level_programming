d62f8464c4267b64668aca1ea0733e0710557770
hailemichael ayana <hmichaelmulugeta@gmail.com>
2022-09-13 19:46:38 -0700

    ques 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * add - add two integers
+ * @x: first input
+ *
+ * @y: second input
+ *
+ * Return: sum of x & y
+ */
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+
+}
