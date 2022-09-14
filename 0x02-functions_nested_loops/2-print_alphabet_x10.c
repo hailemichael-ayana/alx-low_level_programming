407d13c2b73c8f12e70fcbc311a4629729e5a984
hailemichael ayana <hmichaelmulugeta@gmail.com>
2022-09-13 18:21:03 -0700

    ques 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int letter, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+	_putchar('\n');
+	}
+}
