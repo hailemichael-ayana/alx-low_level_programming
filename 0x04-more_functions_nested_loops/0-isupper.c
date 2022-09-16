c270277c4fc2ca1408eb36dc172b34f027b27673
hailemichael ayana <hmichaelmulugeta@gmail.com>
2022-09-15 17:56:21 -0700

    ques 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,18 @@
 /**
  * _isupper - check if c is upper
  *
- * @c: input 
+ * @c: input
  *
  * Return: 1 if its uppercase, 0 for other
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
