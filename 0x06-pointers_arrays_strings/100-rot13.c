904a4c655160cfe4e44f737d89b8c1c04ef1e73f
hailemichael ayana <hmichaelmulugeta@gmail.com>
2022-09-22 16:32:40 -0700

    ques 8 (advanced 1)

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - encode characters
+ *
+ * @arr: array of characters
+ *
+ * Return: arr
+ */
+char *rot13(char *arr)
+{
+	char ordered[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rand[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
+	int j = 0;
+
+	while (arr[i] != '\0')
+	{
+		for (j = 0; orderd[j] != '\0' && arr[i] != ordered[j]; j++)
+			;
+		if (j < 52)
+		{
+			arr[i] = rand[j];
+		}
+		i++;
+	}
+	return (arr);
+}
