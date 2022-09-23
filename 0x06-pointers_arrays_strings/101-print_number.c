a00ebccf437123d5a94931a0ba10941afc1df848
hailemichael ayana <hmichaelmulugeta@gmail.com>
2022-09-22 19:33:46 -0700

    ques 9 (advanced 2)

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,22 +10,5 @@
 
 void print_number(int n)
 {
-	unsigned int a, b, x;
 
-	if (n < 0)
-	{
-		_putchar(45);
-		a = n * -1;
-	}
-	else
-		a = n;
-	b = a;
-	x = 1;
-	while (b > 9)
-	{
-		b /= 10;
-		x *= 10;
-	}
-	for (; x >= 1; x /= 10)
-		_putchar(((a / x) % 10) + 48);
 }
