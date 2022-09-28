feda6ce4d6f68cd3622e1211068fa79bd5d32463
hailemichael ayana <hmichaelmulugeta@gmail.com>
2022-09-28 01:33:30 -0700

    ques 7 (advanced 1)

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,55 @@
+#include "main.h"
+/**
+ * _strlen_recursion - length str
+ *
+ * @str: pointer
+ *
+ * Return: int
+ */
+int _strlen_recursion(char *str)
+{
+
+	int x;
+
+	if (*str == '\0')
+		return (0);
+	str++;
+	x = 1 + _strlen_recursion(str);
+	return (x);
+}
+/**
+ * _palindrome - function
+ *
+ * @i: intger
+ *
+ * @l: integer
+ *
+ * @p: pointer
+ *
+ * Return: integer
+ */
+int _palindrome(int l, int i, char *p)
+{
+	if (i > l / 2)
+		return (1);
+	else if (p[i] != p[l - i - 1])
+		return (0);
+	else
+		return (_palindrome(l, i + 1, p));
+}
+/**
+ * is_palindrome - function
+ *
+ * @s: pointer
+ *
+ * Return: int
+ */
+int is_palindrome(char *s)
+{
+	int x;
+	int y;
+
+	x = _strlen_recursion(s);
+	y = _palindrome(x, 0, s);
+	return (y);
+}
