5f803e3c4be3f7d16be582f13c8516b9755d30c6
hailemichael ayana <hmichaelmulugeta@gmail.com>
2022-09-30 12:37:30 -0700

    ques 5 (advanced 1)

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,20 +14,31 @@
  */
 int main(int argc, char *argv[])
 {
-	int num, digit, sum = 0;
+	int a, n = 0, i, t;
+	int c[5] = {25, 10, 5, 2, 1};
 
-	for (num = 0; num <= argc; num++)
+	if (argc != 2)
 	{
-		for (digit = 0; argv[num][digit]; digit++)
-		{
-			if (argv[num][digit] < '0' || argv[num][digit] > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		sum += atoi(argv[num]);
+		puts("Error");
+		return (1);
 	}
-	printf("%d\n", sum);
+	a = atoi(argv[1]);
+	if (a <= 0)
+	{
+		puts("0");
+		return (1);
+	}
+	else
+	{
+		for (i = 0; i < 5; i++)
+		{
+			t = a / c[i];
+			a -= t * c[i];
+			n += t;
+			if (a == 0)
+				break;
+		}
+	}
+	printf("%d\n", n);
 	return (0);
 }
