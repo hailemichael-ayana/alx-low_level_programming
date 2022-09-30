8412b278aac2725c5620947cf05886ddf1a226bc
hailemichael ayana <hmichaelmulugeta@gmail.com>
2022-09-30 12:01:24 -0700

    ques 1 and main.h modified

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *
+ *@argc: count of the arguments supplied to the program
+ *
+ *@argv: array of pointers to the strings
+ *
+ *Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
