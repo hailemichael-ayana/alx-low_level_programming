b27d61d0a19bc7ab67112aaab10ee7f465f3e051
hailemichael ayana <hmichaelmulugeta@gmail.com>
2022-09-30 12:30:02 -0700

    ques 5 (advanced 1)

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 {
 	int num, digit, sum = 0;
 
-	for (num = 1; num < argc; num++)
+	for (num = 0; num <= argc; num++)
 	{
 		for (digit = 0; argv[num][digit]; digit++)
 		{
