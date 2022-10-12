f79f3478b67d17ba5e3c8d6999cf10d6c44d2927
hailemichael ayana <hmichaelmulugeta@gmail.com>
2022-10-12 00:05:56 -0700

    ques 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - execute a function given as a parameter
+ *
+ * @array: pointer
+ *
+ * @size: unsigned
+ *
+ * @action: function pointer
+ *
+ * Return: Always 0
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
