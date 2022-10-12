720305e5492ffb1eed369c4e8be25dd4e4059727
hailemichael ayana <hmichaelmulugeta@gmail.com>
2022-10-11 23:59:22 -0700

    readme, function_pointers.h, ques 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ * print_name - print the name
+ *
+ * @name: string
+ *
+ * @f: function to print with
+ *
+ * Return:Always 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != '\0' && f != '\0')
+		f(name);
+}
