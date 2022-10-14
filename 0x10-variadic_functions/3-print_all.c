4d792926f98df5e611983a7262a5046a606442b5
hailemichael ayana <hmichaelmulugeta@gmail.com>
2022-10-13 17:32:51 -0700

    ques 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,14 +10,13 @@
 void print_all(const char *const format, ...)
 {
 	va_list valist;
-	unsigned int i = 0, j, c = 0;
+	unsigned int i = 0, j = 0, c = 0;
 	char *str;
 	const char t_arg[] = "cifs";
 
 	va_start(valist, format);
 	while (format && format[i])
 	{
-		j = 0;
 		while (t_arg[j])
 		{
 			if (format[i] == t_arg[j] && c)
@@ -43,6 +42,7 @@ void print_all(const char *const format, ...)
 				if (!str)
 				{
 					printf("(nil)");
+					break;
 				}
 				printf("%s", str);
 		}
