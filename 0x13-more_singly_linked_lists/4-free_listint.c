b7d0c18b59d6af224fc4536474659fb3ae7f5db5
hailemichael-ayana <hmichaelmulugeta@gmail.com>
2022-10-25 11:17:35 -0700

    ques 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+
+
+/**
+ *
+ * free_listint - free list
+ *
+ * @head: pointer to head
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while (head != NULL)	
+		next = hea->next;
+		free(head);
+		head = next;
+	}
+}
