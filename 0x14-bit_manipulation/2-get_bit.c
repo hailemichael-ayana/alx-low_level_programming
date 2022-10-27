c7c954b072737a0ec399e085498dfb683605e358
hailemichael-ayana <hmichaelmulugeta@gmail.com>
2022-10-27 22:13:29 +0300

    ques 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *get_bit - get the value of a bit at a given index
+ *
+ *@n: integer
+ *
+ *@index: position of index 
+ *
+ *Return: value of a bit
+ */
+
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned int p;
+if (index >= 32)
+return (-1);
+p = 1 << index;
+p &= n;
+p >>= index;
+
+return (p);
+}
