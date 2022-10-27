fc4cad697867fa714ca5a5200efa352e35c43e68
hailemichael-ayana <hmichaelmulugeta@gmail.com>
2022-10-27 22:24:44 +0300

    ques 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+
+/**
+ *findbits - count number of bits to be flipped
+ *
+ *@a:input
+ *
+ *Return: number of bits
+ */
+
+unsigned int findbits(unsigned long int a)
+{
+long int i = 0;
+
+
+while (a)
+{
+i += a & 1;
+a >>= 1;
+}
+return (i);
+}
+
+/**
+ *flip_bits - bits need to flip from number to another
+ *@n: unsigned integer
+ *@m: unsigned integer
+ *
+ *Return: number of bit to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+return (findbits(n ^ m));
+}
